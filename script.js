0b5a550d26279e59cb0461f5bdd69902cbd6d6d2
judeallanhill <judeallanhill@icloud.com>
2023-07-22 14:06:46 -0700

    Fixing ELIES ISSUES, OF DEALIING OTHER PEOPLES COD

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function fillBounds(name) {
     ctx.fillRect(name.bounds.x, name.bounds.y, name.bounds.w, name.bounds.h);
 }
 editor.value = localStorage.runnercode_text;
+
 if ( localStorage.runnercode_new !== "false" ) {
     localStorage.runnercode_text = "";
     localStorage.runnercode_new = "false";
@@ -22,7 +23,26 @@ if ( localStorage.runnercode_new !== "false" ) {
 function log(d) {
     return(d);
 }
+document.getElementById("export").addEventListener("click", function() {
+    log("Hello")
+        const htmlContent = editor.value;
+        const fileName = 'game.js';
 
+        // Create a Blob with the HTML content
+        const blob = new Blob([htmlContent], { type: 'text/html' });
+
+        // Create a URL for the Blob
+        const url = URL.createObjectURL(blob);
+
+        // Create an anchor element with download attribute and click it
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = fileName;
+        a.click();
+
+        // Release the URL object
+        URL.revokeObjectURL(url);
+}); 
 function deleteCanvas() {
     canvas.style.visibility = "hidden";
 }
