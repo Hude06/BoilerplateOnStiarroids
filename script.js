0abe55e5dda2ff3a772affb9bf1df9e67c637ed6
judeallanhill <judeallanhill@icloud.com>
2023-07-22 09:00:40 -0700

    Adding download button

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 canvas.style.visibility = "hidden";
+function log(d) {
+    return(d)
+}
 editor.value = localStorage.runnercode_text;
 if ( localStorage.runnercode_new !== "false" ) {
     localStorage.runnercode_text = "";
     localStorage.runnercode_new = "false";
 }
+document.getElementById("export").addEventListener("click", function() {
+    log("Hello")
+        const htmlContent = editor.value;
+        const fileName = 'game.js';
 
-function log(d) {
-    return(d)
-}
+        // Create a Blob with the HTML content
+        const blob = new Blob([htmlContent], { type: 'text/html' });
+
+        // Create a URL for the Blob
+        const url = URL.createObjectURL(blob);
+
+        // Create an anchor element with download attribute and click it
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = fileName;
+        a.click();
+
+        // Release the URL object
+        URL.revokeObjectURL(url);
+}); 
 function deleteCanvas() {
     canvas.style.visibility = "hidden";
 }
