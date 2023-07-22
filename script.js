4e96caac891cbaed50d89c2710c0d59074484e5b
ECIHO1 <133424865+ECIHO1@users.noreply.github.com>
2023-07-22 17:00:56 -0400

    Added style and save button

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ function log(d) {
     return(d)
 }
 function fillRect(x,y,w,h) {
-    ctx.fillRect(x,y,w,h)
+    ctx.fillRect(x,y,w,h);
+}
+
+function fillBounds(name) {
+    ctx.fillRect(name.bounds.x, name.bounds.y, name.bounds.w, name.bounds.h);
 }
 editor.value = localStorage.runnercode_text;
 if ( localStorage.runnercode_new !== "false" ) {
@@ -16,22 +20,19 @@ if ( localStorage.runnercode_new !== "false" ) {
 }
 
 function log(d) {
-    return(d)
+    return(d);
 }
+
 function deleteCanvas() {
     canvas.style.visibility = "hidden";
 }
+
 function createCanvas(width, height) {
     canvas.style.visibility = "visible";
     canvas.height = height;
     canvas.width = width;
 }
 
-window.onbeforeunload = closingCode;
-function closingCode(){
-    localStorage.runnercode_text = editor.value;
-   return null;
-}
 function clearCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
@@ -39,9 +40,13 @@ function clearCanvas() {
 window.addEventListener("keydown", function (event) {
     currentKey.set(event.key, true);
     keydown = true;
-  });
+});
 
-  window.addEventListener("keyup", function (event) {
+window.addEventListener("keyup", function (event) {
     currentKey.set(event.key, false);
     keydown = false;
-  });
+});
+
+saveButton.addEventListener("click", function(){
+    localStorage.runnercode_text = editor.value;
+});
